ba0090db4e20a6144fcb16137c497db80767e792
liquidz <liquidz.uo@gmail.com>
2015-05-11 12:30:06 +0900

    update persona to use docomo dialogue api

--- project.clj
+++ project.clj
@@ -7,7 +7,8 @@
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/tools.namespace "0.2.10"]
                  [org.clojure/data.zip "0.1.1"]
-                 [clj-http "1.0.1"]
+                 [clj-http "1.1.2"]
+                 [clj-docomo-dialogue "0.0.2"]
                  [jubot "0.1.1"]]
 
   :uberjar-name "panpan-standalone.jar"
--- src/panpan/persona.clj
+++ src/panpan/persona.clj
@@ -1,11 +1,13 @@
 (ns panpan.persona
   (:require
     [jubot.handler   :as handler]
-    [jubot.scheduler :as scheduler]
-    [jubot.brain     :as brain]))
+    [jubot.brain     :as brain]
+    [clj-docomo-dialogue.core :as dd]))
 
-(defn persona-handler
-  "p 何食べたい？ - 食べたいものを決めてもらう"
+(def ^:const CONTEXT_KEY "docomo_dialogue_context")
+(def ^:private api-key (System/getenv "DOCOMO_API_KEY"))
+
+(defn persona-hear-handler
   [{:keys [user] :as arg}]
   (handler/regexp
     arg
@@ -22,3 +24,15 @@
       (rand-nth ["牛丼" "パスタ" "カレー" "寿司" "蕎麦" "うどん" "定食"
                  "ファミレス" "ラーメン" "チャーハン" "餃子" "ハンバーグ"
                  "竹" "笹" "タケノコ" "豆腐" "オムライス"]))))
+
+(defn panpan-handler
+  [{:keys [text message-for-me?] :as arg}]
+  (when message-for-me?
+    (handler/regexp arg
+      #".+"
+      (fn [& _]
+        (when-let [res (some-> api-key
+                               (dd/talk text {:t 30
+                                              :context (brain/get CONTEXT_KEY)}))]
+          (brain/set CONTEXT_KEY (:context res))
+          (:utt res))))))
