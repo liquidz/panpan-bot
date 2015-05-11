69dbcc3d118bde5f894c65b748770cba5421a95a
liquidz <liquidz.uo@gmail.com>
2015-05-11 13:31:58 +0900

    update vim handler

--- src/panpan/vim.clj
+++ src/panpan/vim.clj
@@ -4,7 +4,8 @@
     [clj-http.client :as client]
     [jubot.adapter   :as ja]
     [jubot.brain     :as jb]
-    [jubot.scheduler :as js]))
+    [jubot.scheduler :as js]
+    [jubot.handler   :as jh]))
 
 (def ^:const NAME "シル")
 (def ^:const ICON "https://dl.dropboxusercontent.com/u/14918307/slack_icon/syr.png")
@@ -13,10 +14,23 @@
 (def ^:private out #(do (ja/out (apply str %&) :as NAME :icon-url ICON) nil))
 
 (def ^:const MESSAGES
-  ["Vim の新しいパッチがきているようですよ"
-   "知ってます？Vim のパッチがきてますよ"
-   "あっ、Vimのパッチがきてるみたいです"
-   ])
+  {:new-version
+   ["Vim の新しいパッチがきているようですよ"
+    "知ってます？Vim のパッチがきてますよ"
+    "あっ、Vimのパッチがきてるみたいです"
+    ]
+   :no-new-version
+   ["新しいパッチは無いみたいです"
+    "ごめんなさい。新しいパッチないです"
+    "んー、特に新しいパッチはないですね"
+    ]
+   :response
+   ["どういたしまして(^^)"
+    "いえいえ(^^)"
+    "役に立ったなら良かったです！"
+    "お役に立ててなりよりです！"
+    ]
+   })
 
 (defn get-latest-patch
   []
@@ -24,18 +38,26 @@
       (str/split #"\s+" 3)))
 
 (defn check-vim-version
-  []
+  [& {:keys [user else?]}]
   (let [[_ version message] (get-latest-patch)
         last-version        (jb/get KEY)]
-    (when (not= version last-version)
-      (jb/set KEY version)
-      (out (rand-nth MESSAGES)
-           "\n" version ": " message))))
+    (if (not= version last-version)
+      (do (jb/set KEY version)
+          (out (if user (str "@" user " ") "")
+               (-> MESSAGES :new-version rand-nth)
+               "\n" version ": " message))
+      (when else?
+        (out (if user (str "@" user " ") "")
+             (-> MESSAGES :no-new-version rand-nth))))))
 
-;(defn vim-version-handler
-;  [{text :text}]
-;  (when (re-find #"vim" text)
-;    (check-vim-version)))
+(defn vim-version-handler
+  [{:keys [user] :as arg}]
+  (jh/regexp arg
+    #"シル(さん)?.+ありがと"
+    (fn [& _] (->> MESSAGES :response rand-nth (out "@" user " ")))
+
+    #"シル(さん)?.+(vim|Vim)"
+    (fn [& _] (check-vim-version :user user :else? true))))
 
 (def vim-version-check-schedule
   (js/schedules
